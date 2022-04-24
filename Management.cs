969076c5f7b128250b6d47c1605ed5be0ad45f69
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-24 19:03:33 +0530

    Review the Count of Review Present for each Product ID

---
+++ Management.cs
@@ -0,0 +1,25 @@
+﻿using ProductReviewManagementwithLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementwithLINQ
+{
+    internal class Management
+    {
+        public void RetrieveCountofRecord(List<ProductReview> reviews)
+        {
+            var RecordData = reviews.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+
+
+            foreach (var list in RecordData)
+            {
+                Console.WriteLine("Product ID" + list.ProductID + "......................." + "Count :" + list.Count);
+            }
+
+        }
+
+    }
+}
