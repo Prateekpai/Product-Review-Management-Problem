9ab2fb40ee79e9b8138da8f9860f82d66d2137d3
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-24 19:08:45 +0530

    Retrieve only Product ID

---
+++ Management.cs
@@ -0,0 +1,27 @@
+﻿using ProductReviewManagementwithLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementwithLINQ
+{
+    internal class Management
+    {
+
+        public void ShowProductIDAndReview(List<ProductReview> reviews)
+        {
+            var RecordData = (from products in reviews
+                              orderby products.ProductID
+                              select products).ToList();
+
+
+            foreach (var list in RecordData)
+            {
+                Console.WriteLine("Product ID" + list.ProductID + "......................." + "Review  :" + list.Review);
+            }
+        }
+
+    }
+}
