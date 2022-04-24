e3fa67e1cc2e4c4939cf8f165ac7132078f31df6
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-24 19:34:36 +0530

    Skip Top 5 Records

---
+++ Management.cs
@@ -0,0 +1,26 @@
+﻿using ProductReviewManagementwithLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementwithLINQ
+{
+    internal class Management
+    {
+        public void SkipTop5Record(List<ProductReview> reviews)
+        {
+            var recordData = (from productreview in reviews
+                              orderby productreview.Rating descending
+                              select productreview).Skip(5);
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product ID" + list.ProductID + "User ID :" + list.Userid + " Rating : " + list.Rating + "Review : " + list.Rating + "Is Like : " + list.IsLike);
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using ProductReviewManagementwithLINQ;
+using System;
+using System.Data;
+namespace ProductReviewManagementwithLinq
+{
+    public class program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Product Review Management Problem");
+
+            List<ProductReview> productreview = new List<ProductReview>()
+            {
+                new ProductReview() { ProductID = 1, Userid = 1, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 2, Userid = 2, Rating = 4, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 3, Userid = 3, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 4, Userid = 4, Rating = 3, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 5, Userid = 5, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 6, Userid = 6, Rating = 1, Review = "Bad", IsLike=false },
+                new ProductReview() { ProductID = 7, Userid = 7, Rating = 1, Review = "Bad", IsLike = false },
+                new ProductReview() { ProductID = 8, Userid = 7, Rating = 3, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 9, Userid = 7, Rating = 4, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 10, Userid = 7, Rating = 5, Review = "Nice", IsLike = true }
+
+            };
+
+
+            Management management = new ProductReviewManagementwithLINQ.Management();
+
+            management.SkipTop5Record(productreview);
+
+        }
+    }
+}
